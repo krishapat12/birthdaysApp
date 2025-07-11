332d8472b78b5472cabb6d9d7ac15fab931819a0
Krisha Patel <patel.krisha.edu@gmail.com>
2025-07-11 09:29:22 -0500

    Initial Commit

---
+++ birthdaysApp/birthdaysAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  birthdaysAppApp.swift
+//  birthdaysApp
+//
+//  Created by Scholar on 7/11/25.
+//
+
+import SwiftUI
+
+@main
+struct birthdaysAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
