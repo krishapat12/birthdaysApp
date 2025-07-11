38feb40bcccca0b757af080f8656eb8d8bd6e97c
Krisha Patel <patel.krisha.edu@gmail.com>
2025-07-11 10:31:42 -0500

    fixed code and did work in class

--- birthdaysApp/ContentView.swift
+++ birthdaysApp/ContentView.swift
@@ -8,16 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        @State private var friends: [friend] = [
+            friend(name: "John", birthday: .now),
+            friend(name: "Samantha", birthday: Date(timeIntervalSince1970: 0)) ]
+    @State private var newName = ""
+    @State private var newBirthday = Date.now
+            var body: some View {
+                NavigationStack {
+                    
+                    List(friends,id: \.name) { friend in
+                        HStack{
+                            Text(friend.name)
+                            Spacer()
+                            Text(friend.birthday,
+                                 format: .dateTime.month(.wide).day().year())
+                        }
+                    }
+                    .navigationTitle("Birthdays App")
+                    .safeAreaInset(edge: .bottom) {
+                        VStack(alignment: .center, spacing: 20){
+                            Text("New Birthday")
+                                .font(.headline)
+                            DatePicker(selection: $newBirthday, in: Date.distantPast...Date.now, displayedComponents: .date) {
+                                TextField("Name", text: $newName)
+                                    .textFieldStyle(.roundedBorder)
+                            }
+                            Button("Save me") {
+                                let newFriend = friend(name: newName, birthday: newBirthday)
+                                friends.append(newFriend)
+                            }
+                            .padding()
+                            .background(.bar)
+                        }
+                    }
+                }
+            }
         }
-        .padding()
-    }
-}
+   
+
+            //not used yet
+    
 
 #Preview {
     ContentView()
---
+++ birthdaysApp/friend.swift
@@ -0,0 +1,21 @@
+//
+//  friend.swift
+//  birthdaysApp
+//
+//  Created by Scholar on 7/11/25.
+//
+
+import Foundation
+
+
+class friend{
+    var name: String
+    var birthday: Date
+    
+    init(name: String, birthday: Date) {
+        self.name = name
+        self.birthday = birthday
+        
+    }
+    
+}
